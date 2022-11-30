dc2534e69b18a91ce823124fb27c7bb4709c2996
mecarm <melecarmine99@gmail.com>
2022-11-30 12:27:14 +0100

    edit layout

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="d-flex justify-content-between p-3 align-items-center">
-      <h1 class="text-danger">NETFLIX</h1>
+      <h1 class="text-danger ps-3">NETFLIX</h1>
       <div class="input-group w-auto p-3">
         <input @keyup.enter="$emit('emitSearch', inputUtente)" type="text" class="form-control" placeholder="Titolo, film, serie" aria-label="Recipient's username"
           aria-describedby="button-addon2" v-model="inputUtente">
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -22,7 +22,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    main{
-        height: 90%;
-    }
+main {
+    height: 90%;
+}
+
+main::-webkit-scrollbar-track {
+    border: 1px solid #000;
+    padding: 2px 0;
+    background-color: #404040;
+}
+
+main::-webkit-scrollbar {
+    width: 10px;
+}
+
+main::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, .3);
+    background-color: #737272;
+    border: 1px solid #000;
+}
 </style>
